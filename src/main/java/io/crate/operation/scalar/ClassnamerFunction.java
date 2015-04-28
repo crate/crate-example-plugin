fb5472d9c9eb0068462effd0b078c66456848c10
Sebastian Utz <su@rtme.net>
2015-04-28 10:50:32 +0200

    initial commit

---
+++ src/main/java/io/crate/operation/scalar/ClassnamerFunction.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.operation.scalar;
+
+import io.crate.metadata.FunctionIdent;
+import io.crate.metadata.FunctionInfo;
+import io.crate.metadata.Scalar;
+import io.crate.operation.Input;
+import io.crate.planner.symbol.Function;
+import io.crate.planner.symbol.Symbol;
+import io.crate.types.DataType;
+import io.crate.types.DataTypes;
+import net.rtme.Classnamer;
+import org.apache.lucene.util.BytesRef;
+
+import java.util.Collections;
+
+public class ClassnamerFunction extends Scalar<BytesRef, Void> {
+
+    public static final String NAME = "classnamer";
+
+    protected final static FunctionInfo INFO = new FunctionInfo(
+            new FunctionIdent(NAME, Collections.<DataType>emptyList()), DataTypes.STRING,
+            FunctionInfo.Type.SCALAR, false);
+
+    public static void register(ScalarFunctionModule module){
+        module.register(new ClassnamerFunction());
+    }
+
+    @Override
+    public BytesRef evaluate(Input[] args) {
+        return new BytesRef(Classnamer.generate());
+    }
+
+    @Override
+    public FunctionInfo info() {
+        return INFO;
+    }
+
+    @Override
+    public Symbol normalizeSymbol(Function symbol) {
+        /* There is no evaluation here, so the function is executed
+           per row. Else every row would contain the same random value*/
+        return symbol;
+    }
+}
---
+++ src/main/java/io/crate/plugin/ExamplePlugin.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to CRATE Technology GmbH ("Crate") under one or more contributor
+ * license agreements.  See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership.  Crate licenses
+ * this file to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.  You may
+ * obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * However, if you have executed another commercial license agreement
+ * with Crate these terms will supersede the license and you may use the
+ * software solely pursuant to the terms of the relevant commercial agreement.
+ */
+
+package io.crate.plugin;
+
+import io.crate.operation.scalar.ClassnamerFunction;
+import io.crate.operation.scalar.ScalarFunctionModule;
+import org.elasticsearch.common.logging.ESLogger;
+import org.elasticsearch.common.logging.Loggers;
+import org.elasticsearch.common.settings.Settings;
+
+public class ExamplePlugin extends AbstractPlugin {
+
+    private static final ESLogger LOGGER = Loggers.getLogger(ExamplePlugin.class);
+
+    public ExamplePlugin(Settings settings) {
+        LOGGER.info("ExamplePlugin loaded");
+    }
+
+    @Override
+    public String name() {
+        return "example-plugin";
+    }
+
+    @Override
+    public String description() {
+        return "A example plugin demonstrating crate's plugin infrastructure.";
+    }
+
+    /**
+     * Each plugin can provide a hook on already loaded modules.
+     * In this example we want to create a {@link io.crate.metadata.Scalar} function which will
+     * be available via SQL statements. Scalar functions can be registered on the
+     * {@link io.crate.operation.scalar.ScalarFunctionModule}, to do so we must implement
+     * the relevant <tt>onModule(AnyModule module)</tt> method.
+     */
+    public void onModule(ScalarFunctionModule module) {
+        ClassnamerFunction.register(module);
+    }
+}
